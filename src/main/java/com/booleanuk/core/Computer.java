43781c31c0bf06620e572e801e18301f6de5e414
Enock97 <enockladu@hotmail.com>
2024-01-19 10:45:26 +0100

    Did some parts of the extension

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -3,14 +3,22 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 
 public class Computer {
-    public ArrayList<Game> installedGames = new ArrayList<>();
+    private ArrayList<Game> installedGames = new ArrayList<>();
     private PowerSupply powerSupply;
+    private Game game;
 
     public Computer(PowerSupply powerSupply) {
         this.installedGames = new ArrayList<>();
+        this.game = new Game("Pokemon");
         this.powerSupply = powerSupply;
     }
     public Computer(PowerSupply powerSupply, ArrayList<Game> installedGames) {
+        this.installedGames.addAll(installedGames);
+        this.powerSupply = powerSupply;
+    }
+
+    public Computer(PowerSupply powerSupply, Game game, ArrayList<Game> installedGames) {
+        this.game = game;
         this.installedGames = installedGames;
         this.powerSupply = powerSupply;
     }
@@ -32,5 +40,29 @@ public class Computer {
             }
         }
         return "Game not installed";
+    }
+
+    public ArrayList<Game> getInstalledGames() {
+        return installedGames;
+    }
+
+    public void setInstalledGames(ArrayList<Game> installedGames) {
+        this.installedGames = installedGames;
+    }
+
+    public PowerSupply getPowerSupply() {
+        return powerSupply;
+    }
+
+    public void setPowerSupply(PowerSupply powerSupply) {
+        this.powerSupply = powerSupply;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
     }
 }
